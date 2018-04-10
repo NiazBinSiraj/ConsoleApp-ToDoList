161c3bb3f43fccc84d882d85de525cac6808370d
NiazBinSiraj <niazbinsiraj@gmail.com>
2018-04-10 15:28:23 +0600

    create welcome.txt file

--- source-code.cpp
+++ source-code.cpp
@@ -1,8 +1,26 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
+
+void Welcome()
+{
+    char s[50];
+    freopen("welcome.txt","r",stdin);
+    while(gets(s)!=NULL)
+      puts(s);
+}
+
+
+void Add()
+{
+    freopen("input-message.txt","r",stdin);
+    char s[50];
+    gets(s);
+    puts(s);
+}
 
 int main()
 {
-
-  return 0;
+    Welcome();
+    Add();
+    return 0;
 }
