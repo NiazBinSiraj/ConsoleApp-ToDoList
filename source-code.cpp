9741b59f6b3c5820d7fa9c684d46c7e25be40af4
NiazBinSiraj <niazbinsiraj@gmail.com>
2018-04-10 21:57:56 +0600

    Add section done

--- source-code.cpp
+++ source-code.cpp
@@ -1,26 +1,77 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void Welcome()
+int counter()
 {
-    char s[50];
-    freopen("welcome.txt","r",stdin);
-    while(gets(s)!=NULL)
-      puts(s);
+    FILE *p;
+    int cnt;
+    p = fopen("counter.txt","r");
+    fscanf(p,"%d",&cnt);
+    fclose(p);
+
+    p = fopen("counter.txt","w");
+    fprintf(p,"%d",cnt+1);
+    fclose(p);
+    return cnt;
+}
+
+void welcome()
+{
+    FILE *p;
+    char s[100];
+    p = fopen("welcome.txt","r");
+    while(fgets(s,100,p)!=NULL)
+      printf("%s",s);
+    fclose(p);
 }
 
 
-void Add()
+int select()
 {
-    freopen("input-message.txt","r",stdin);
-    char s[50];
+    FILE *p;
+    char s[100];
+    p = fopen("select.txt","r");
+    while(fgets(s,100,p)!=NULL)
+      printf("%s",s);
+    fclose(p);
+
+    int action;
+    scanf("%d",&action);
+    return action;
+}
+
+
+void add()
+{
+    FILE *p;
+    char s[5000];
+    p = fopen("input-message.txt","r");
+    fgets(s,100,p);
+    printf("%s",s);
+    fclose(p);
+
     gets(s);
-    puts(s);
+    gets(s);
+    p = fopen("list.txt","a");
+    fprintf(p,"%d. %s\n\n",counter()+1,s);
+    fclose(p);
+
+
+    return;
 }
+
 
 int main()
 {
-    Welcome();
-    Add();
+
+    welcome();
+
+    while(true)
+    {
+        int a = select();
+        if(a == 1) add();
+        else if(a==4) break;
+    }
+
     return 0;
 }
