4205a52d7fd7da92df4d739a50012719c2dbd5ed
NiazBinSiraj <niazbinsiraj@gmail.com>
2018-04-10 22:20:33 +0600

    show section done

--- source-code.cpp
+++ source-code.cpp
@@ -1,5 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define MX 100000
 
 int counter()
 {
@@ -23,6 +24,7 @@ void welcome()
     while(fgets(s,100,p)!=NULL)
       printf("%s",s);
     fclose(p);
+    return;
 }
 
 
@@ -47,29 +49,39 @@ void add()
     char s[5000];
     p = fopen("input-message.txt","r");
     fgets(s,100,p);
-    printf("%s",s);
+    printf("%s\n",s);
     fclose(p);
 
     gets(s);
     gets(s);
     p = fopen("list.txt","a");
-    fprintf(p,"%d. %s\n\n",counter()+1,s);
+    fprintf(p,"%d. %s\n",counter()+1,s);
     fclose(p);
-
-
     return;
 }
 
+void show()
+{
+    printf("List:\n");
+    FILE *p;
+    char s[MX];
+    p = fopen("list.txt","r");
+    while(fgets(s,MX,p)!=NULL)
+      printf("%s",s);
+    fclose(p);
+    printf("\n");
+    return;
+}
 
 int main()
 {
-
     welcome();
 
     while(true)
     {
         int a = select();
         if(a == 1) add();
+        else if(a == 2) show();
         else if(a==4) break;
     }
 
